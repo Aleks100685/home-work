503d89aa4ac846ff3b58158711ff0a7a53db97bb
Sasha <linvest21@yandex.ru>
2024-02-09 20:14:30 +0700

    home work

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,12 +6,12 @@ class TestProductAndCategory(unittest.TestCase):
     def test_product_initialization(self):
         product = Product("Ноутбук", "Мощный ноутбук", 999.99, 5)
         self.assertEqual(product.name, "Ноутбук")
-        # Добавьте больше утверждений на основе структуры вашего класса Product
+
 
     def test_category_initialization(self):
         category = Category("Электроника", "Электронные устройства")
         self.assertEqual(category.name, "Электроника")
-        # Добавьте больше утверждений на основе структуры вашего класса Category
+
 
 if __name__ == "__main__":
     unittest.main()
